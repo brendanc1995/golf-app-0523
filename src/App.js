88362ef4cd2677613e0c61757348780923398a98
Brendan Conway <brendanc1995@gmail.com>
2023-05-18 16:04:51 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from "react";
-import CourseItem from "./CourseItem";
-import sampleData from "./sampleData";
+
 
 function App() {
-  const courseData = sampleData.map((data) => <CourseItem data={data}/>)
 
-  //const skiInfo = skiData.map((info) => <Product info={info}/>)
 
 
   return (
     <div className="App">
     <h1>This is the beginning of my golf app</h1>
-    {courseData}
     
 
     </div>
