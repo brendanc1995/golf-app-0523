5592ccab3bd5898477a56b118328c1d99c0d3df7
Brendan Conway <brendanc1995@gmail.com>
2023-05-18 14:24:51 -0400

    Removed Server

--- src/App.js
+++ src/App.js
@@ -1,32 +1,9 @@
 import React, {useEffect, useState} from "react";
 
 function App() {
-
-  const [courses, setCourses] = useState([{
-    courseName: '',
-    hole1: '',
-    hole2: ''
-  }])
-
-  useEffect(() => {
-    fetch("/course").then(res => {
-      if(res.ok){
-        return res.json()
-      }
-    }).then(jsonRes => setCourses(jsonRes))
-  })
-
-
-  return (
+return (
     <div className="App">
     <h1>This is the beginning of my golf app</h1>
-    {courses.map(course =>
-      <div>
-        <h1>{course.name}</h1>
-        <h1>{course.hole1}</h1>
-        <h1>{course.hole2}</h1>
-      </div>
-    )}
     </div>
   );
 }
