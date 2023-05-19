c3f1b25f7c8453743899ad6c2e42d46a48de852e
Brendan Conway <brendanc1995@gmail.com>
2023-05-18 21:39:49 -0400

    test

--- src/CourseInfoContainer.js
+++ src/CourseInfoContainer.js
@@ -2,17 +2,14 @@ import React from "react";
 import CourseInfo from "./CourseInfo";
 import courseData from "./courseData";
 
-export default function CourseInfoContainer(){
-     const course = courseData.map((data) => <CourseInfo data={data}/>)
+export default function CourseInfoContainer() {
+  const course = courseData.map((data) => <CourseInfo data={data} />);
 
-    return(
-        <div>
-            <h1>Hello</h1>
-            {course}
-            <CourseInfo />
-        </div>
-    )
-
-
-
+  return (
+    <div>
+      <h1>Hello</h1>
+      {course}
+      <CourseInfo />
+    </div>
+  );
 }
