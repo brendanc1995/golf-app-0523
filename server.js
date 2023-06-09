903618e5e74831de669d7ca4ce385d67e9026195
Brendan Conway <brendanc1995@gmail.com>
2023-06-09 14:37:41 -0400

    cleanup code

--- server.js
+++ server.js
@@ -1,25 +1,18 @@
-// Import necessary modules
 const express = require('express');
 const Course = require('./models/courses');
 const app = express();
 const mongoose = require('mongoose');
 
-// Connect to MongoDB
 mongoose.connect('mongodb+srv://brendanc1995:con130509@golfcourse.tlkbtoq.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true });
 
-// Set view engine
 app.set('view engine', 'ejs');
 
-// Middleware for parsing request bodies
 app.use(express.urlencoded({ extended: false }));
 
-// Define route for root path ("/")
 app.get('/', async (req, res) => {
   try {
-    // Fetch data from MongoDB
     const courses = await Course.find();
 
-    // Render the view template with the data
     res.json(courses)
   } catch (error) {
     console.error(error);
@@ -27,10 +20,8 @@ app.get('/', async (req, res) => {
   }
 });
 
-// Establish MongoDB connection
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
 
-// Start the server
 app.listen(5000, () => console.log('Server Started'));
