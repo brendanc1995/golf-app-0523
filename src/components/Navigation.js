2765f5e06afdf53e5c7ceb9c5e2998da3956b68e
Brendan Conway <brendanc1995@gmail.com>
2023-06-09 17:13:13 -0400

    beginning on navigation styling and functionality

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,14 +3,28 @@ import {Link} from "react-router-dom"
 
 
 export default function Navigation(){
-    return(
-    <>
+  const [menu, setMenu] = React.useState(false)
+
+  function openMenu(){
+    setMenu(prevState => !prevState)
+  }
+  function closeMenu(){
+    setMenu(false)
+  }
+
+var navUl = document.getElementsByClassName("nav-ul")[0]
+console.log(navUl.offsetHeight)
+
+  
+  return(
+    <div>
       <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/courses">Courses</Link></li>
+        <i class="bi bi-list menu" onClick={openMenu}></i>
+        <ul className={`nav-ul ${menu === true ? "open" : ""}`}>
+          <li><Link to="/" onClick={closeMenu}>Home</Link></li>
+          <li><Link to="/courses" onClick={closeMenu}>Courses</Link></li>
         </ul>
       </nav>
-    </>
+    </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import { BrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import './index.css'
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
